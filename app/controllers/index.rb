2c965261940836dfb1a1233372be0f6266ca06b5
JEFF <justaboutjeff@gmail.com>
2013-06-18 18:17:10 -0500

    adds twitter login and tweet shield

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,6 @@ get '/' do
 end
 
 get '/sign_in' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
   redirect request_token.authorize_url
 end
 
@@ -13,11 +12,8 @@ get '/sign_out' do
 end
 
 get '/auth' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-  # at this point in the code is where you'll need to create your user account and store the access token
   user = User.find_or_create_by_oauth_token(username: @access_token.params[:screen_name],
                      oauth_token: @access_token.params[:oauth_token],
                      oauth_secret: @access_token.params[:oauth_token_secret])
